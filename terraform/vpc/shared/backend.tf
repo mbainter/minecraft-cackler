c2fc1bf8d64fd4a3d51baeeeb6d6b03533576270
Mark Bainter <mark@litmus.com>
2021-02-08 01:21:33 -0600

    add shared vpc config

---
+++ terraform/vpc/shared/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "bainterfam"
+
+    workspaces {
+      name = "vpc-shared"
+    }
+  }
+}
---
+++ terraform/vpc/shared/main.tf
@@ -0,0 +1,18 @@
+module "default_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.70.0"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24"]
+  public_subnets  = ["10.10.100.0/24", "10.10.101.0/24", "10.10.102.0/24"]
+
+  name = "shared"
+  cidr = "10.10.0.0/16"
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+  }
+}
