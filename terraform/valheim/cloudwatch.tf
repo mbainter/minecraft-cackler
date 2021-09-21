aef6d6e4726d18eeac0c9f2fbc778cc5ee913986
Mark Bainter <mark@litmus.com>
2021-09-21 14:01:54 -0500

    add logs for lambdas

--- terraform/valheim/cloudwatch.tf
+++ terraform/valheim/cloudwatch.tf
@@ -7,3 +7,25 @@ resource "aws_cloudwatch_log_group" "valheim" {
     Application = "Valheim"
   }
 }
+
+resource "aws_cloudwatch_log_group" "valheim_mordheim" {
+  name = "valheim/ecs/mordheim"
+
+  retention_in_days = 3
+
+  tags = {
+    Application = "Valheim"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "valheim_lambda" {
+  for_each = toset(["start", "stop"])
+
+  name = "/aws/lambda/valheim-${each.key}"
+
+  retention_in_days = 3
+
+  tags = {
+    Application = "Valheim"
+  }
+}
