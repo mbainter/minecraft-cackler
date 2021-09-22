56bd3cd7bc414998de672a819176c2ad142d0ff2
Mark Bainter <mark@litmus.com>
2021-09-21 21:19:43 -0500

    allow outbound connections

--- terraform/valheim/security_groups.tf
+++ terraform/valheim/security_groups.tf
@@ -22,6 +22,28 @@ resource "aws_security_group_rule" "valheim_allow_games" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "security_group_icmp_type_3_in" {
+  security_group_id = aws_security_group.valheim.id
+  description       = "ICMP destination unreachable messages inbound"
+  type              = "ingress"
+  protocol          = "icmp"
+  #tfsec:ignore:AWS006
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 3
+  to_port     = -1
+}
+
+resource "aws_security_group_rule" "security_group_icmp_type_11_in" {
+  security_group_id = aws_security_group.valheim.id
+  description       = "ICMP destination unreachable messages inbound"
+  type              = "ingress"
+  protocol          = "icmp"
+  #tfsec:ignore:AWS006
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 11
+  to_port     = 0
+}
+
 resource "aws_security_group_rule" "valheim_allow_status" {
   security_group_id = aws_security_group.valheim.id
   description       = "Allow status connections to Valheim instances"
@@ -31,5 +53,41 @@ resource "aws_security_group_rule" "valheim_allow_status" {
   to_port   = var.status_port
   protocol  = "tcp"
   #tfsec:ignore:AWS006
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "valheim_allow_tcp_outbound" {
+  security_group_id = aws_security_group.valheim.id
+  description       = "Allow outbound TCP connections"
+
+  type      = "egress"
+  from_port = 0
+  to_port   = 65535
+  protocol  = "tcp"
+  #tfsec:ignore:AWS007
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "valheim_allow_udp_outbound" {
+  security_group_id = aws_security_group.valheim.id
+  description       = "Allow outbound UDP connections"
+
+  type      = "egress"
+  from_port = 0
+  to_port   = 65535
+  protocol  = "udp"
+  #tfsec:ignore:AWS007
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "valheim_allow_icmp_outbound" {
+  security_group_id = aws_security_group.valheim.id
+  description       = "Allow outbound icmp connections"
+
+  type      = "egress"
+  from_port = -1
+  to_port   = -1
+  protocol  = "icmp"
+  #tfsec:ignore:AWS007
   cidr_blocks = ["0.0.0.0/0"]
 }
